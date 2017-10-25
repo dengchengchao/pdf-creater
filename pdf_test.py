39c1462ae5ba6b16ee03541a2be4e2923862f781
Citron <dengchengchao@outlook.com>
2017-10-25 17:00:26 +0800

    merge origin master

--- pdf_test.py
+++ pdf_test.py
@@ -1,5 +1,9 @@
 import pdf_parser
+import pdf_xmler
 test=pdf_parser.pdf_parser('result.xml')
 print(len(test.xml_list))
 for node in test.xml_list:
-    print(node.line_point)
+    list1=[]
+    test2=pdf_xmler.block(node)
+    for node2 in test2:
+        print(node2.point.left)
